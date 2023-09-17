72d22f34f63677182bc59f1dba056549a3b1be62
DenysVM <muratov.d@gmail.com>
2023-09-17 20:11:56 +0200

    Mentor 1.2 - hiding LoadMore button

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,118 +1,113 @@
-import React, { Component } from "react";
-import fetchImages from "../services/Api/Api";
-import Searchbar from "../components/Searchbar/Searchbar";
-import ImageGallery from "../components/ImageGallery/ImageGallery";
-import Button from "../components/Button/Button";
-import Modal from "../components/Modal/Modal";
-import CustomLoader from "../components/Loader/Loader";
-import { toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+  import React, { Component } from "react";
+  import fetchImages from "../services/Api/Api";
+  import Searchbar from "../components/Searchbar/Searchbar";
+  import ImageGallery from "../components/ImageGallery/ImageGallery";
+  import Button from "../components/Button/Button";
+  import Modal from "../components/Modal/Modal";
+  import CustomLoader from "../components/Loader/Loader";
+  import { toast } from "react-toastify";
+  import "react-toastify/dist/ReactToastify.css";
 
-class App extends Component {
-  state = {
-    query: "", 
-    page: 1,
-    images: [],
-    selectedImage: null,
-    isLoading: false,
-  };
+  class App extends Component {
+    state = {
+      query: "", 
+      page: 1,
+      images: [],
+      selectedImage: null,
+      isLoading: false,
+    };
 
-  handleQueryChange = (event) => {
-     this.setState({ query: event.target.value });
-  };
+    handleQueryChange = (event) => {
+      this.setState({ query: event.target.value });
+          };
 
-  handleSearch = async () => {
+    handleSearch = async () => {
 
-    this.setState({ page: 1, images: [], isLoading: true });
+      this.setState({ page: 1, images: [], isLoading: true });
+
+      try {
+        await this.fetchImagesData();
+      } catch (error) {
+        console.error("Error:", error);
+        this.setState({ isLoading: false });
+        throw error; 
+      }
+    };
+
+    handleImageClick = (imageUrl) => {
+      this.setState({ selectedImage: imageUrl });
+    };
+
+    handleCloseModal = () => {
+      this.setState({ selectedImage: null });
+    };
+
+  fetchImagesData = async () => {
+    if (!this.state.query) return;
+
+    this.setState({ isLoading: true });
 
     try {
-      await this.fetchImagesData();
+      const newImages = await fetchImages(
+        this.state.query,
+        this.state.page
+      );
+
+      if (newImages.length === 0) {
+        toast.error("No images found for your search.", {
+          position: "top-right",
+          autoClose: 3000,
+        });
+      } else {
+        
+        this.setState((prevState) => ({
+          images: [...prevState.images, ...newImages],
+        }));
+      }
     } catch (error) {
-      console.error("Error:", error);
-      this.setState({ isLoading: false });
-      throw error; 
-    }
-  };
-
-  handleImageClick = (imageUrl) => {
-    this.setState({ selectedImage: imageUrl });
-  };
-
-  handleCloseModal = () => {
-    this.setState({ selectedImage: null });
-  };
-
-  componentDidMount() {
-    
-    this.fetchImagesData();
-  }
-
-fetchImagesData = async () => {
-  if (!this.state.query) return;
-
-   this.setState({ isLoading: true });
-
-  try {
-    const newImages = await fetchImages(
-      this.state.query,
-      this.state.page
-    );
-
-    if (newImages.length === 0) {
-      toast.error("No images found for your search.", {
+      toast.error("An error occurred while searching. Please try again later.", {
         position: "top-right",
         autoClose: 3000,
       });
-    } else {
-      
-      this.setState((prevState) => ({
-        images: [...prevState.images, ...newImages],
-      }));
+    } finally {
+      this.setState({ isLoading: false });
     }
-  } catch (error) {
-    toast.error("An error occurred while searching. Please try again later.", {
-      position: "top-right",
-      autoClose: 3000,
-    });
-  } finally {
-    this.setState({ isLoading: false });
-  }
-};
+  };
 
-  handleLoadMore = () => {
-  this.setState(
-    (prevState) => ({ page: prevState.page + 1, isLoading: true }),
-    () => {
-      this.fetchImagesData();
-    }
-  );
-};
-
-  render() {
-    return (
-      <div>
-        <Searchbar
-          query={this.state.query}
-          onQueryChange={this.handleQueryChange}
-          onSubmit={this.handleSearch}
-        />
-        <ImageGallery
-          images={this.state.images}
-          onImageClick={this.handleImageClick}
-        />
-        {this.state.images.length > 0 && (
-          <Button onClick={this.handleLoadMore} />
-        )}
-        {this.state.isLoading && <CustomLoader />}
-        {this.state.selectedImage && (
-          <Modal
-            image={this.state.selectedImage}
-            onClose={this.handleCloseModal}
-          />
-        )}
-      </div>
+    handleLoadMore = () => {
+    this.setState(
+      (prevState) => ({ page: prevState.page + 1, isLoading: true }),
+      () => {
+        this.fetchImagesData();
+      }
     );
-  }
-}
+  };
 
-export default App;
+    render() {
+      return (
+        <div>
+          <Searchbar
+            query={this.state.query}
+            onQueryChange={this.handleQueryChange}
+            onSubmit={this.handleSearch}
+          />
+          <ImageGallery
+            images={this.state.images}
+            onImageClick={this.handleImageClick}
+          />
+          {this.state.images.length > 11 && (
+            <Button onClick={this.handleLoadMore} />
+          )}
+          {this.state.isLoading && <CustomLoader />}
+          {this.state.selectedImage && (
+            <Modal
+              image={this.state.selectedImage}
+              onClose={this.handleCloseModal}
+            />
+          )}
+        </div>
+      );
+    }
+  }
+
+  export default App;
